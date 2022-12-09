24774ef3daa860a264799ee8d9504b6d587379e9
Sam <samwrighthwk@gmail.com>
2022-12-09 20:21:23 +0000

    Setup for ejs

---
+++ part2/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile('/pages/index.html', {root: __dirname});
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile('/pages/about.html', {root: __dirname});
+})
+
+app.get('/about-me', (req, res) => {
+    res.redirect('/about');
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile('/pages/404.html', { root: __dirname });
+})
