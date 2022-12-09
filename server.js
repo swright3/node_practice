1bd634c144719919d2e6624d4e7bc58bced3088b
Sam <samwrighthwk@gmail.com>
2022-12-09 14:49:33 +0000

    Sending back status codes

--- server.js
+++ server.js
@@ -9,17 +9,25 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
     fs.readFile(path, (err, data) => {
-        err ? console.log(err) : res.end(data);
+        if (err) {
+            console.log(err);
+            res.end()
+        } else {
+            res.end(data);
+        }
     })
 })
 
