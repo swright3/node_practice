8f378490ef67df5b576494a11d179623e627fe16
Sam <samwrighthwk@gmail.com>
2022-12-09 13:42:59 +0000

    Basic serving of html

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Received request')
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello world</h1>');
+    res.end();
 })
 
 server.listen(3000, 'localhost', () => {
-    console.log('Listening on port 3000')
+    console.log('Listening on port 3000');
 })
