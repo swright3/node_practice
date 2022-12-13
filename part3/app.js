3cedc78c896869372b5380259b7f48d98fa78172
Sam <samwrighthwk@gmail.com>
2022-12-13 19:51:09 +0000

    Using morgan for dev details

--- part3/app.js
+++ part3/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -9,13 +10,7 @@ app.listen(3000);
 // register view engine
 app.set('view engine', 'ejs');
 
-app.use((req, res, next) => {
-  console.log('new request made');
-  console.log('host: ' + req.hostname);
-  console.log('path: ' + req.path);
-  console.log('method: ' + req.method);
-  next();
-});
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   const blogs = [
